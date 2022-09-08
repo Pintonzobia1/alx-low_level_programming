4d532307535329e2222ec741f6ac23ad9343bd28
Pintonzobia1 <pintonzobia@gmail.com>
2022-09-08 14:28:13 -0700

    5th commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,47 +1,11 @@
-nclude <stdio.h>
-
-
+#include <studio.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
-
+* main-entry point for the program.
+* Return:0 if no errors, return non zero if errors
+*/
 int main(void)
-
 {
-
-		char acharacter;
-
-			int ainteger;
-
-				long along;
-
-					long long alonglong;
-
-						float afloat;
-
-
-
-							printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-
-								printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-
-									printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-
-										printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-
-											printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
-
-
-
-												return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }

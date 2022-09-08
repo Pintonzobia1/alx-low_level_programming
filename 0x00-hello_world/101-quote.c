c9e788910558505d8f74c263182d2aee0556fbbf
Pintonzobia1 <pintonzobia@gmail.com>
2022-09-08 15:46:05 -0700

    100th commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Prints a text 
+ * main - Prints a text
  *
  * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	write(2, "and the piece of airt is useful\" -Dora Korpar, 2015-10-19\n",59);
+	write(2, "and the piece of airt is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

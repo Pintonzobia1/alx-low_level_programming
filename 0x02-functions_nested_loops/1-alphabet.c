221e43d33e5bdd818c5fd682b4475771031b33ae
Pintonzobia1 <pintonzobia@gmail.com>
2022-09-13 13:25:11 -0700

    .

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet - print the alphabet in lowercase, followed by a new line
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+}
+

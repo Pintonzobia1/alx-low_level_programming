46827af26c2ae47010304d6ad46882a275d68258
Pintonzobia1 <pintonzobia@gmail.com>
2022-09-14 09:15:18 -0700

    100th commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,16 +2,12 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- *
  * @n: number of the times table
- *
  */
-
 void print_times_table(int n)
 {
 	int i, j, k;
-
-	if (n >= 0 && n <= 15)
+		if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -46,9 +42,7 @@ void print_times_table(int n)
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
-
 			}
 			_putchar('\n');
 		}
 	}
-}

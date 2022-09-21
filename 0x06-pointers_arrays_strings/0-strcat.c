e2f67a5118a71211c3d762024ab17341ac07e66b
Pintonzobia1 <pintonzobia@gmail.com>
2022-09-21 16:15:10 -0700

    new push

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,44 +1,12 @@
+#include <string.h>
 #include "main.h"
-
-#include <stdio.h>
-
-
-
 /**
-
- * _strcat - writes function that concatenates two strings
-
- * @dest: first string
-
- * @src: string to be concatenated
-
- *
-
- * Return: dest
-
+ * _strcat - concatenates two strings
+ * @dest: a string pointer
+ * @src: another string pointer
+ * Return: a string pointer
  */
-
 char *_strcat(char *dest, char *src)
-
 {
-
-	int len1, len2, i;
-
-
-
-	len1 = strlen(dest);
-
-	len 2 = strlen(src);
-
-
-
-	for (i = 0; i <= len2; i++)
-
-{
-
-	dest[len1 + i] = src[i];
-
+	return (strcat(dest, src));
 }
-
-return (dest);
-
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,13 @@
+#include <string.h>
 #include "main.h"
-
 /**
  * _strncat - concatenates two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
+ * @dest: the destination buffer
+ * @src: the source buffer
+ * @n: the total number of bytes to concatenate from src to dest
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int m;
-	int i;
-
-	m = 0;
-
-	for (i = 0; i < 1000; i++)
-	{
-		if (dest[i] == '\0')
-		{
-			break;
-		}
-		m++;
-	}
-
-	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[m + i] = src[i];
-	}
-	dest[m + i] = '\0';
-	return (dest);
+	return (strncat(dest, src, n));
 }

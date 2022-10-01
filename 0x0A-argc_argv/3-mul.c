904951ec9ce1af6c0fd0a5423bbaebf0824e710d
Pintonzobia1 <pintonzobia@gmail.com>
2022-09-30 20:49:39 -0700

    commit all

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array nane
+ * Return: 1 if not enough arguments passed in, 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
